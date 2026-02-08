3194f6cc1660704e56bafb62e629048d4f68de39
Israah Hanfa <israahhanfa@Israahs-MacBook-Air.local>
2026-02-08 22:41:11 +0530

    Fix CORS with wildcard

--- backend/main.py
+++ backend/main.py
@@ -25,10 +25,7 @@ app = FastAPI(
 # ✅ THEN ADD CORS (IMPORTANT ORDER)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://burnaware.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
